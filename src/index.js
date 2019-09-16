3118241b844193cbed33e65d574d8dc8d9650fc5
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-09-16 16:52:51 -0400

    finished routing

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+
 ReactDOM.render(
 <Router><App /></Router>, document.getElementById('root'));
